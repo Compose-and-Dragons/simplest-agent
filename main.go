63e9bc5d5c556b81b033e9dd95da9bcedce3ab67
k33g <ph.charriere@gmail.com>
2025-07-17 10:20:15 +0200

    🛟 Updated.

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/budgies-nest/budgie/agents"
+	"github.com/openai/openai-go"
+)
+
+func main() {
+
+	modelRunnerBaseUrl := os.Getenv("MODEL_RUNNER_BASE_URL")
+
+	if modelRunnerBaseUrl == "" {
+		panic("MODEL_RUNNER_BASE_URL environment variable is not set")
+	}
+	modelRunnerChatModel := os.Getenv("MODEL_RUNNER_MODEL")
+	fmt.Println("Using Model Runner Chat Model:", modelRunnerChatModel)
+
+	if modelRunnerChatModel == "" {
+		panic("MODEL_RUNNER_MODEL environment variable is not set")
+	}
+
+	bob, err := agents.NewAgent("bob_agent",
+		agents.WithDMR(modelRunnerBaseUrl),
+		agents.WithParams(openai.ChatCompletionNewParams{
+			Model:       modelRunnerChatModel,
+			Temperature: openai.Opt(0.5),
+			Messages: []openai.ChatCompletionMessageParamUnion{
+				openai.SystemMessage("You are Dungeon Master Bob, a Dwarf in a fantasy world. You are friendly and helpful, but you can also be mischievous. You have the following attributes:"),
+				openai.UserMessage("Tell mee a story about a Werewolf in the forest."),
+			},
+		}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = bob.ChatCompletionStream(context.Background(), func(self *agents.Agent, content string, err error) error {
+		fmt.Print(content)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+}
